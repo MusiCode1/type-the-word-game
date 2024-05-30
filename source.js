acdefdab3d82994c6b3d6336ba6ac085802ef41b
MusiCode <musicode3@gmail.com>
2024-05-31 00:14:02 +0300

    הגדלת אינפוט

--- source.js
+++ source.js
@@ -1,3 +1,4 @@
+const audio = new Audio('https://cdn.freesound.org/previews/352/352527_3428684-lq.mp3');
 
 class CustomCard extends HTMLElement {
     constructor() {
@@ -19,22 +20,29 @@ class CustomCard extends HTMLElement {
     }
 }
 
-const audio = new Audio('https://cdn.freesound.org/previews/352/352527_3428684-lq.mp3');
+function adjustCardWidth(letters) {
+    const img = document.querySelector('img');
+    const imgWidth = img.clientWidth;
+    const cardMargin = 10; // Adjust this value as needed
+    const totalCardMargin = cardMargin * (letters.length - 1);
+    const cardWidth = ((imgWidth - totalCardMargin) / letters.length) - 50;
 
+    const cards = document.querySelectorAll('custom-card');
+    cards.forEach(card => {
+        card.setWidth(cardWidth);
+    });
+}
 
-document.addEventListener('DOMContentLoaded',async function () {
-
-    var templates = document.createElement('template')
-    templates.innerHTML = await (await fetch('./card.html')).text()
-    document.body.appendChild(templates.content.cloneNode(true))
+document.addEventListener('DOMContentLoaded', async function () {
+    var templates = document.createElement('template');
+    templates.innerHTML = await (await fetch('./card.html')).text();
+    document.body.appendChild(templates.content.cloneNode(true));
 
     customElements.define('custom-card', CustomCard);
 
     const correctWord = 'מים';
-
     const letters = correctWord.split('');
     const container = document.getElementById('cards-container');
-    const img = document.querySelector('img');
 
     letters.forEach(letter => {
         const card = document.createElement('custom-card');
@@ -42,36 +50,29 @@ document.addEventListener('DOMContentLoaded',async function () {
         container.appendChild(card);
     });
 
-    function adjustCardWidth() {
-
-        const imgWidth = img.clientWidth;
-        const cardMargin = 10; // Adjust this value as needed
-        const totalCardMargin = cardMargin * (letters.length - 1);
-        const cardWidth = (imgWidth - totalCardMargin) / letters.length;
-
-        const cards = document.querySelectorAll('custom-card');
-        cards.forEach(card => {
-            card.setWidth(cardWidth);
-        });
-    }
-
+    const img = document.querySelector('img');
     img.src = 'https://www.smartcut.co.il/wp-content/uploads/2020/05/%D7%91%D7%A8-%D7%9E%D7%99%D7%9D-%D7%97%D7%9B%D7%9D.png';
 
-    window.addEventListener('load', adjustCardWidth);
-    window.addEventListener('resize', adjustCardWidth);
+    const imgWidth = img.clientWidth;
 
-    document.getElementById('user-input').addEventListener('input', function () {
-        const input = this.value;
-        const inputLength = input.length;
+    adjustCardWidth(letters)
 
-        if (input === correctWord) {
+    window.addEventListener('load', () => adjustCardWidth(letters));
+    window.addEventListener('resize', () => adjustCardWidth(letters));
+
+    /** @type {HTMLInputElement} */
+    const input = document.getElementById('user-input');
+
+    input.style.width = `${imgWidth - 10}px`;
+
+    input.addEventListener('input', function () {
+        const inputValue = this.value;
+
+        if (inputValue === correctWord) {
             this.classList.remove('incorrect');
             this.classList.add('correct');
-            // Play sound
             audio.play();
-
-
-        } else if (correctWord.startsWith(input)) {
+        } else if (correctWord.startsWith(inputValue)) {
             this.classList.remove('incorrect');
             this.classList.remove('correct');
         } else {
@@ -79,10 +80,4 @@ document.addEventListener('DOMContentLoaded',async function () {
             this.classList.add('incorrect');
         }
     });
-})
-
-
-    (async function () {
-        //get the imported document in templates:
-
-    })()
+});
