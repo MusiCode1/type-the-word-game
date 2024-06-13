8bc8ca84a16ede6df1f76f4699f9ada6e3d229bf
MusiCode <musicode3@gmail.com>
2024-06-14 00:18:15 +0300

    הוספת הבהוב באותיות

--- src/card-latter-element.js
+++ src/card-latter-element.js
@@ -1,8 +1,21 @@
 import { getTemplate } from "./util.js";
+
 export class LetterCard extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+    }
+
+    static get observedAttributes() {
+        return ['correct'];
+    }
+
+    set correct(value) {
+        this.setAttribute('correct', value);
+    }
+
+    get correct() {
+        return this.getAttribute('correct');
     }
 
     async connectedCallback() {
@@ -14,6 +27,20 @@ export class LetterCard extends HTMLElement {
 
         const letter = this.getAttribute('letter');
         this.shadowRoot.querySelector('.card').textContent = letter;
+
+        this.dispatchEvent(new CustomEvent('card-loaded', {}))
+    }
+
+    attributeChangedCallback(attrName, oldValue, newValue) {
+        if (attrName === 'correct') {
+            const card = this.shadowRoot.querySelector('.card');
+
+            if (newValue === 'true') {
+                card.classList.add('correct');
+            } else {
+                card?.classList.remove('correct');
+            }
+        }
     }
 
 }
--- src/main-page-code.js
+++ src/main-page-code.js
@@ -10,12 +10,13 @@ async function main() {
 
     await setMainTemplate();
 
-    setCards(word);
+    const cards = await setCards(word);
 
     setImage();
 
     const audio = setAudio();
     setInput(word, audio);
+    setCurrentCard(word, cards);
 
     onResize();
     window.onresize = onResize;
@@ -26,13 +27,17 @@ async function main() {
 
     const observer = new MutationObserver((mutations) => {
 
-        if (timeout) {
-            clearTimeout(timeout);
-            timeout = null;
-        }
+        mutations.forEach((mutation) => {
 
-        timeout = setTimeout(onResize, 0);
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
 
+            timeout = setTimeout(onResize, 0);
+
+            //console.log(mutation);
+        });
     });
 
     observer.observe(document.body, {
@@ -40,12 +45,9 @@ async function main() {
         childList: true,
         subtree: true
     });
-
-
-
 }
 
-function setDisplay(){
+function setDisplay() {
 
     const mainContent = document.querySelector('#main-content');
     mainContent.classList.add('display');
@@ -74,9 +76,21 @@ async function setMainTemplate() {
     body.appendChild(templateContent);
 }
 
+/**
+ * Sets the cards for the correct word.
+ *
+ * @param {string} correctWord - The correct word to generate cards for
+ * @return {Promise<Record<string, HTMLElement>>} An object containing the generated cards
+ */
 function setCards(correctWord) {
 
     customElements.define('letter-card', LetterCard);
+
+    /** @type {Record<string, HTMLElement>} */
+    const lettersCardsObject = {};
+
+    /**@type {Promise<void>[]} */
+    const waitForCardsLoad = [];
 
     const letters = correctWord.split('');
     const container = document.querySelector('.card-container');
@@ -84,8 +98,20 @@ function setCards(correctWord) {
     letters.forEach(letter => {
         const card = document.createElement('letter-card');
         card.setAttribute('letter', letter);
+        card.setAttribute('correct', 'false');
         container.appendChild(card);
+
+        lettersCardsObject[letter] = card;
+
+        waitForCardsLoad.push(new Promise(resolve => {
+            card.addEventListener('card-loaded', resolve);
+        }));
     });
+
+    return Promise.all(waitForCardsLoad)
+        .then(
+            () => lettersCardsObject
+        );
 
 }
 
@@ -110,6 +136,84 @@ function setInput(correctWord, audio) {
         }
     });
 
+}
+
+/**
+ * Sets the current card based on user input.
+ *
+ * @param {string} correctWord - The correct word for the game
+ * @param {HTMLElement[]} cards - Array of card elements
+ * @return {void}
+ */
+function setCurrentCard(correctWord, cards) {
+    const input = document.getElementById('user-input');
+
+    const correctWordArr = correctWord.split('');
+
+    const letter = correctWordArr[0];
+
+    setCorrectAttributes(letter);
+
+    input.addEventListener('input', () => {
+        const correctLetter =
+            getCorrectLetter(correctWord, input);
+
+        if (correctLetter == false) {
+            const letter = correctWordArr[0];
+
+            setCorrectAttributes(letter);
+
+        } else if (correctLetter !== true) {
+
+            const correctLetterIndex =
+                correctWordArr.indexOf(correctLetter);
+
+            const nextLetter = correctWordArr[correctLetterIndex + 1];
+
+            setCorrectAttributes(nextLetter);
+
+        } else {
+
+            setCorrectAttributes("");
+        }
+
+        console.log(correctLetter);
+
+    });
+
+    function setCorrectAttributes(correctLetter) {
+        Object.entries(cards).forEach(
+            ([letter, card]) => {
+                if (letter === correctLetter) {
+                    card.setAttribute('correct', 'true');
+                } else {
+                    card.setAttribute('correct', 'false');
+                }
+            }
+        );
+    }
+}
+/**
+ * 
+ * @param {string} correctWord 
+ * @returns 
+ */
+function getCorrectLetter(correctWord, input) {
+
+    const inputValue = input.value.trim();
+
+    if (!inputValue) {
+        return false;
+    }
+    else if (correctWord.startsWith(inputValue)) {
+        return inputValue.split('').pop();
+
+    } else if (inputValue === correctWord) {
+        return true;
+
+    } else {
+        return correctWord.split('')[0];
+    }
 }
 
 function onResize() {
@@ -144,7 +248,7 @@ function resizeElement(element) {
         elementHeight = element.clientHeight,
         widthToHeightRatio = elementWidth / elementHeight;
 
-    console.log('[View Width]', viewWidth, '[View Height]', viewHeight);
+    //console.log('[View Width]', viewWidth, '[View Height]', viewHeight);
 
     let idealHeight = (viewHeight - 20);
     let idealWidth = idealHeight * widthToHeightRatio;
@@ -156,9 +260,9 @@ function resizeElement(element) {
 
     const newMargin = ((viewWidth - idealWidth) / 2);
 
-    console.log('idealWidth', idealWidth, 'idealHeight', idealHeight);
-
-    console.log('newMargin', newMargin);
+    /*     console.log('idealWidth', idealWidth, 'idealHeight', idealHeight);
+    
+        console.log('newMargin', newMargin); */
 
     return {
         margin: Math.floor(newMargin),
